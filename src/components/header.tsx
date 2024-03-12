9cd37011b673603a82d50f2ed80ad6029340644a
Davi Job <davi.job@hotmail.com>
2024-03-12 17:03:56 -0300

    INIT: initial commit + header
    Project initial files, configs and settings
    
    Header section added

---
+++ src/components/header.tsx
@@ -0,0 +1,29 @@
+export interface HeaderProps {}
+
+export function Header(props: HeaderProps) {
+    return (
+        <header>
+            <h1 className="header__title">Levels Gym</h1>
+
+            <nav className="header__nav">
+                <ul>
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">About Us</a>
+                    </li>
+                    <li>
+                        <a href="#">Trainers</a>
+                    </li>
+                    <li>
+                        <a href="#">Pricing Plans</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact Us</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
